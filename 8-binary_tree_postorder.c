f59a3f388004c9af20c7c270214b98c0a3c9d1c7
Noziop <contact@fassih.ch>
2024-08-06 16:30:36 +0200

    EDIT 8-binary_tree_postorder : correction, with order to sort the value of nodes that were reverted

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,12 +11,12 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	/* Recursively traverse the right subtree */
-	binary_tree_postorder(tree->right, func);
+	/* Recursively traverse the left subtree */
+	binary_tree_postorder(tree->left, func);
 
 	/* Visit the root node */
 	func(tree->n);
 
-	/* Recursively traverse the left subtree */
-	binary_tree_postorder(tree->left, func);
+	/* Recursively traverse the right subtree */
+	binary_tree_postorder(tree->right, func);
 }
